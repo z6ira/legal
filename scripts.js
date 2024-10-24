a8e28f1290d87b1bec7f2f87960b1ba843101c62
z6ira <zaaira2022@gmail.com>
2024-10-24 16:44:46 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const observador = new IntersectionObserver( (entrada) =>{ 
+    entrada.forEach ((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('apareceu')
+        } else {
+            entry.target.classList.remove('apareceu')
+        }
+    })
+})
+
+const elementos = document.querySelectorAll('.escondido')
+
+elementos.forEach((element) => observador.observe(element))
+
+
+
+
+
+
+
+
+const images = document.querySelectorAll('.image');
+let currentIndex = 0;
+
+function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+}
+
+// Inicializa a primeira imagem como ativa
+images[currentIndex].classList.add('active');
+
+// Troca de imagem a cada 3 segundos
+setInterval(showNextImage, 3000);
+
